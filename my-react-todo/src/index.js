3ca406515262fef0b42308d25592e65807887766
addykumar27 <adarshna27@yahoo.com>
2017-05-02 10:03:18 -0700

    adding header component and created js files

--- my-react-todo/src/index.js
+++ my-react-todo/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {Router, browserHistory} from 'react-router'
+import routes from './config/routes.js'
+
 
 ReactDOM.render(
-  <App />,
+ <Router routes={routes} history={browserHistory}/>,
   document.getElementById('root')
 );
