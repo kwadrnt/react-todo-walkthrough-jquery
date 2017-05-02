b6cafed51c08830c754c09af72fc277bf578f953
kwadrnt <kevinctse@gmail.com>
2017-05-02 10:49:58 -0700

    finished sprint 2

--- my-react-todo/src/App.js
+++ my-react-todo/src/App.js
@@ -7,9 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        {this.props.children}
       </div>
 
     );
-  } 
+  }
 }
 export default App;
